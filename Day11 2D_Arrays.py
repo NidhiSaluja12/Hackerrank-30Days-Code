81d4facc7fc0ddf36ddcbf5de2c1aa9e6ecfd417
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-06-12 22:43:00 +0530

    Create Day11 2D_Arrays.py

---
+++ Day11 2D_Arrays.py
@@ -0,0 +1,25 @@
+def hourGlassSum(arr):
+    maxSum = -99
+    for i in range (4):
+        for j in range(4):
+            top = sum(arr[i][j:j+3])
+            middle = arr[i+1][j+1]
+            bottom = sum(arr[i+2][j:j+3])
+            hourGlassSum = top+middle+bottom
+            maxSum = max(hourGlassSum, maxSum)
+    return maxSum
+            
+            
+
+
+
+if __name__ == '__main__':
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+        
+    res = hourGlassSum(arr)
+    print(res)
+
