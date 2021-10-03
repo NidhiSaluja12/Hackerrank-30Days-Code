8fb4e79ba13d1413746944d3821835a1733117ff
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-10-03 23:47:13 +0530

    Update Day1 HelloWorld.py

--- Day1 HelloWorld.py
+++ Day1 HelloWorld.py
@@ -1,6 +1,7 @@
-input_string = input()
 
-# Print a string literal saying "Hello, World." to stdout.
+# Print a string literal saying "Hello, World." to stdout
+
+input_string = input()
 
 print('Hello, World.')
 print(input_string)
