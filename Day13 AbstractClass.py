0dab56071d3ca4811dffb33bdd9fd9635af08cec
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-10-11 22:49:13 +0530

    Update Day13 AbstractClass.py

--- Day13 AbstractClass.py
+++ Day13 AbstractClass.py
@@ -1,10 +1,12 @@
 from abc import ABCMeta, abstractmethod
+
 class Book(object, metaclass=ABCMeta):
     def __init__(self,title,author):
         self.title=title
         self.author=author   
     @abstractmethod
     def display(): pass
+
 class MyBook(Book):
     price = 0
     def __init__(self, title, author, price):
